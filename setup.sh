a549e8d77076c9fc19a241a1e2008cef9d5752be
Norman Sue <normful@gmail.com>
2014-08-03 11:46:16 -0700

    fix minor bugs

--- setup.sh
+++ setup.sh
@@ -3,40 +3,51 @@ THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # apt
 sudo apt-get update
 
-# prerequisites
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties ctags
-
-# .gemrc
-cat $THIS_DIR/.gemrc > $HOME/.gemrc
+# packages
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties ctags autojump
 
 # vim
+echo "Setting up neobundle"
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+echo "Creating or replacing .vimrc"
 cat $THIS_DIR/.vimrc > $HOME/.vimrc
-mkdir $HOME/.vim/.cache/swap
-mkdir $HOME/.vim/.cache/undo
-mkdir $HOME/.vim/.cache/backup
+echo "Creating vim swap, undo, and backup folders"
+mkdir -p $HOME/.vim/.cache/swap
+mkdir -p $HOME/.vim/.cache/undo
+mkdir -p $HOME/.vim/.cache/backup
 
 # bash aliases
+echo "Appending to .bash_aliases"
 cat $THIS_DIR/.bash_aliases >> $HOME/.bash_aliases
 echo '. $HOME/.bash_aliases' >> $HOME/.bashrc
 
 # .gitconfig
+echo "Appending to .gitconfig"
 cat $THIS_DIR/.gitconfig >> $HOME/.gitconfig
 
-# rbenv and ruby
+# rbenv
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.bashrc
 echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
 exec $SHELL
-
 git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> $HOME/.bashrc
 exec $SHELL
 
-rbenv install 2.1.2
+# ruby 2.1.2
+CONFIGURE_OPTS="--disable-install-rdoc"
+rbenv install -v 2.1.2
 rbenv global 2.1.2
 ruby -v
 
+# .gemrc
+echo "Creating or replacing .gemrc"
+cat $THIS_DIR/.gemrc > $HOME/.gemrc
+
+# bundler
+gem install bundler -V
+
 # rubocop
-cat $THIS_DIR/.rubocop.yml > $HOME/.rubocop.yml
 gem install rubocop -V
+echo "Creating or replacing global rubocop settings"
+cat $THIS_DIR/.rubocop.yml > $HOME/.rubocop.yml
