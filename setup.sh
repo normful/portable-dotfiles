6595f8c68e05fe87e178081434f471aa7af91954
Norman Sue <normful@gmail.com>
2014-08-12 23:47:26 -0700

    add custom.zsh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # apt
 sudo apt-get update
 
-# packages
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties ctags autojump tree
+# essential packages
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+
+# additional packages
+sudo apt-get install ctags autojump tree zsh
 
 # autojump
 echo '. /usr/share/autojump/autojump.sh' >> $HOME/.bash_profile
@@ -42,7 +45,9 @@ exec $SHELL
 # ruby 2.1.2
 CONFIGURE_OPTS="--disable-install-rdoc"
 rbenv install -v 2.1.2
+rbenv rehash
 rbenv global 2.1.2
+rbenv local 2.1.2
 ruby -v
 
 # .gemrc
@@ -56,3 +61,11 @@ gem install bundler -V
 gem install rubocop -V
 echo "Creating or replacing global rubocop settings"
 cat $THIS_DIR/.rubocop.yml > $HOME/.rubocop.yml
+
+# zsh
+sudo chsh -s $(which zsh)
+
+# oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+echo "Creating or replacing custom.zsh"
+cat $THIS_DIR/.oh-my-zsh/custom/custom.zsh > $HOME/.oh-my-zsh/custom/custom.zsh
