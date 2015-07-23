d161483d499a65713b8acc25414fa3459ede7abf
Norman Sue <normful@gmail.com>
2015-07-22 22:03:47 -0700

    copy in opp.zsh files

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -2,3 +2,12 @@ ZSH_THEME="jreese"
 if [ -f ~/.bash_profile ]; then
     . ~/.bash_profile
 fi
+
+# For vi mode
+# https://github.com/hchbaw/opp.zsh
+if [ -f ~/opp.zsh ]; then
+    bindkey -v
+    source ~/opp.zsh
+    source ~/opp/surround.zsh
+    source ~/opp/textobj-between.zsh
+fi
