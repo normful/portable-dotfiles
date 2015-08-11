22eb8db773dc9dffa3c6c3525723276314f26218
Norman Sue <normful@gmail.com>
2015-08-11 02:57:44 -0700

    more tweaks to minimal setup script

--- minimal-setup.sh
+++ minimal-setup.sh
@@ -1,5 +1,10 @@
 # Note: This script is for a Debian-based distribution
 
+if [ "$EUID" -eq 0 ]; then 
+    echo "Please run this script as a normal user, not the root user"
+    exit
+fi
+
 THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # apt
@@ -14,18 +19,20 @@ echo '. /usr/share/autojump/autojump.sh' >> $HOME/.bash_profile
 # vim
 echo "Setting up neobundle"
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-echo "Creating or replacing .vimrc"
-cat $THIS_DIR/.vimrc-minimal > $HOME/.vimrc
+echo "Symlinking minimal .vimrc"
+ln -s $THIS_DIR/.vimrc-minimal $HOME/.vimrc
 echo "Creating vim swap, undo, and backup folders"
 mkdir -p $HOME/.vim/.cache/swap
 mkdir -p $HOME/.vim/.cache/undo
 mkdir -p $HOME/.vim/.cache/backup
 mkdir -p $HOME/.vim/dict
-cat $THIS_DIR/.vim/dict/node.dict > $HOME/.vim/dict/node.dict
+echo "Symlinking node.dict"
+ln -s $THIS_DIR/.vim/dict/node.dict $HOME/.vim/dict/node.dict
 
 # bash aliases
-echo "Appending to .bash_aliases"
-cat $THIS_DIR/.bash_aliases >> $HOME/.bash_aliases
+echo "Symlinking .bash_aliases"
+rm $HOME/.bash_aliases
+ln -s $THIS_DIR/.bash_aliases $HOME/.bash_aliases
 echo '. $HOME/.bash_aliases' >> $HOME/.bash_profile
 
 # .gitconfig
@@ -34,12 +41,15 @@ cat $THIS_DIR/.gitconfig >> $HOME/.gitconfig
 
 # oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
-echo "Creating or replacing custom.zsh"
-cat $THIS_DIR/.oh-my-zsh/custom/custom.zsh > $HOME/.oh-my-zsh/custom/custom.zsh
+echo "Symlinking custom.zsh"
+rm $HOME/.oh-my-zsh/custom/custom.zsh
+ln -s $THIS_DIR/.oh-my-zsh/custom/custom.zsh $HOME/.oh-my-zsh/custom/custom.zsh
 
 # opp.zsh
+echo "Symlinking opp.zsh"
 ln -s $THIS_DIR/opp $HOME/opp
 ln -s $THIS_DIR/opp.zsh $HOME/opp.zsh
 
 # zsh
+sudo chsh -s /bin/zsh $(whoami)
 echo 'To change default shell to zsh: sudo chsh -s /bin/zsh username'
