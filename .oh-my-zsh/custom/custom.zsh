ed046ccae5b4083be0c4ae3907defd5909c8bb96
Norman Sue <normful@gmail.com>
2016-01-08 18:49:56 -0800

    log into tmux session

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -12,3 +12,8 @@ if [ -f ~/opp.zsh ]; then
     source ~/opp/surround.zsh
     source ~/opp/textobj-between.zsh
 fi
+
+if which tmux > /dev/null 2>&1; then
+   # if not inside a tmux session, and if no session is started, start a new session
+   test -z "$TMUX" && (tmux attach || tmux new-session)
+fi
