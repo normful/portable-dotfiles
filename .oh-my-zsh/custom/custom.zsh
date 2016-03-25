0821f6ac8742eb8eacc49a9cba920d9fa93b480e
Norman Sue <normful@gmail.com>
2016-03-25 02:20:05 -0700

    [zsh] source additional files quietly

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,23 +1,23 @@
 ZSH_THEME="dieter"
 
 if [[ -e ~/.bash_profile ]]; then
-    . ~/.bash_profile
+    . ~/.bash_profile > /dev/null
  fi
 
 if [[ -e ~/.bash_aliases ]]; then
-    . ~/.bash_aliases
+    . ~/.bash_aliases > /dev/null
 fi
 
 if [[ -e ~/.bash_aliases_local ]]; then
-    . ~/.bash_aliases_local
+    . ~/.bash_aliases_local > /dev/null
 fi
 
 # For vi mode https://github.com/hchbaw/opp.zsh
 if [[ -e ~/opp.zsh ]]; then
     bindkey -v
-    source ~/opp.zsh
-    source ~/opp/surround.zsh
-    source ~/opp/textobj-between.zsh
+    source ~/opp.zsh > /dev/null
+    source ~/opp/surround.zsh > /dev/null
+    source ~/opp/textobj-between.zsh > /dev/null
 fi
 
 if which tmux > /dev/null 2>&1; then
