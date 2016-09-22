80a1b3fb24138e5439a3a6ebf24c079639837c4d
Norman Sue <normful@gmail.com>
2016-09-22 02:13:17 -0700

    [zsh] use vim for MANPAGER

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -24,3 +24,6 @@ if which tmux > /dev/null 2>&1; then
    # if not inside a tmux session, and if no session is started, start a new session
    test -z "$TMUX" && (tmux attach || tmux new-session)
 fi
+
+# use vim for reading man pages
+export MANPAGER="col -b | vim -MR - "
